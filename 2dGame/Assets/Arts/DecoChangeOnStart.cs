6b34d092dec45253c252d4c921a4b8bbe4b1e762
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2021-07-10 17:44:25 +0800

    装饰物

--- 2dGame/Assets/Arts/DecoChangeOnStart.cs
+++ 2dGame/Assets/Arts/DecoChangeOnStart.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class DecoChangeOnStart : MonoBehaviour
 {
+    public bool use_changeOnStart;
     public Sprite[] spList;
+    int spIndex;
+
     void Start()
     {
-        this.transform.GetComponent<SpriteRenderer>().sprite = spList[Random.Range(0, spList.Length)];
+        if (use_changeOnStart) RandomSetSp();
+    }
+
+    public int RandomSetSp()
+    {
+        spIndex = Random.Range(0, spList.Length);
+        Refresh();
+        return spIndex;
+    }
+
+    public void SetSp(int index)
+    {
+        if (index >= spList.Length || index < 0) index = 0;
+        spIndex = index;
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        if(spList[spIndex]==null)Destroy(this.transform.parent.gameObject);
+        this.transform.GetComponent<SpriteRenderer>().sprite = spList[spIndex];
     }
 }
