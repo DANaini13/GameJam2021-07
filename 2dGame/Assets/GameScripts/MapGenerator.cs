f8e6fce62d4c0dafada795f499737fc15f302f8f
zeyong shan <danaini13@gmail.com>
2021-07-10 18:09:24 +0800

    后处理push一下

--- 2dGame/Assets/GameScripts/MapGenerator.cs
+++ 2dGame/Assets/GameScripts/MapGenerator.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    
+    private GameObject room_prefab;
+    public float width_interval;
+    public float height_interva;
+    public Vector2Int size;
+
+    private void Awake()
+    {
+        room_prefab = Resources.Load<GameObject>("room_basic");
+        for (int row = 0; row < size.x; ++row)
+        {
+            for (int col = 0; col < size.y; ++col)
+            {
+                
+            }
+        }
+    }
 }
