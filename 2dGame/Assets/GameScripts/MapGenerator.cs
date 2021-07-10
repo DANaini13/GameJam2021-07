8e69bf405ff1fac484c2b8a44ed84fe7c760ef94
zeyong shan <danaini13@gmail.com>
2021-07-10 18:30:58 +0800

    xxx

--- 2dGame/Assets/GameScripts/MapGenerator.cs
+++ 2dGame/Assets/GameScripts/MapGenerator.cs
@@ -12,12 +12,14 @@ public class MapGenerator : MonoBehaviour
 
     private void Awake()
     {
-        room_prefab = Resources.Load<GameObject>("room_basic");
+        room_prefab = Resources.Load("Prefabs/room_baisc") as GameObject;
         for (int row = 0; row < size.x; ++row)
         {
             for (int col = 0; col < size.y; ++col)
             {
-                
+                var position = new Vector3(row * width_interval, col * height_interva, 0);
+                var room = Instantiate(room_prefab, transform);
+                room.transform.position = position;
             }
         }
     }
