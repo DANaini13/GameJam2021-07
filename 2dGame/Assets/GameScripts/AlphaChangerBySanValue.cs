1c9d5873d9bfb272737d700a26d503a7e0ef81d2
zeyong shan <danaini13@gmail.com>
2021-07-10 14:59:43 +0800

    提供alphaChanger组件, 挂上之后透明度随san值降低而升高

---
+++ 2dGame/Assets/GameScripts/AlphaChangerBySanValue.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaChangerBySanValue : MonoBehaviour
+{
+    private SpriteRenderer[] sprite_renders;
+    
+    private void Awake()
+    {
+        sprite_renders = GetComponentsInChildren<SpriteRenderer>();
+        CREventSystem.Instance.ListenCustomeEventByKey(CRCustomEvents.ON_SAN_VALUE_CHANGED, this, OnSanValueChangedEvent);
+    }
+
+    private void Start()
+    {
+        foreach (var sprite_render in sprite_renders)
+        {
+            sprite_render.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    void OnSanValueChangedEvent(object arg)
+    {
+        var typed_arg = (CRCustomArgs.OnSanValueChangedArg) arg;
+        // 通过san值决定透明度
+        float alpha = 1.0f - typed_arg.san;
+        Debug.Log(alpha);
+        foreach (var sprite_render in sprite_renders)
+        {
+            sprite_render.color = new Color(1, 1, 1, alpha);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CREventSystem.EraseCustomeEventByKey(CRCustomEvents.ON_SAN_VALUE_CHANGED, this);
+    }
+}
